89c38df012746a9a7d561a850c670a62882ad950
admin <admin@admins-MacBook-Pro.local>
2019-10-22 21:19:28 +0800

    原生与flutter的侨接

--- android/app/src/main/kotlin/com/example/my_flutter_app/PageRouter.java
+++ android/app/src/main/kotlin/com/example/my_flutter_app/PageRouter.java
@@ -1,4 +1,37 @@
 package com.example.my_flutter_app;
 
-class PageRouter {
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+public class PageRouter {
+    public static final String NATIVE_FIRST_PAGE_URL = "flutterbus://nativeFirstPage";
+    public static final String NATIVE_SECOND_PAGE_URL = "flutterbus://nativeSecondPage";
+    public static final String FLUTTER_FIRST_PAGE_URL = "flutterbus://flutterFirstPage";
+    public static final String FLUTTER_SECOND_PAGE_URL = "flutterbus://flutterSecondPage";
+
+    public static boolean openPageByUrl(Context context, String url) {
+        return openPageByUrl(context, url, 0);
+    }
+
+    public static boolean openPageByUrl(Context context, String url, int requestCode) {
+        try {
+            Intent intent;
+            if (url.startsWith(NATIVE_FIRST_PAGE_URL)) {
+                intent = new Intent(context, FlutterPageActivity.class);
+                intent.putExtra("url", url);
+                context.startActivity(intent);
+                return true;
+            } else if(url.startsWith(FLUTTER_FIRST_PAGE_URL)) {
+                intent = new Intent(context, MainActivity.class);
+                intent.putExtra("url", url);
+                context.startActivity(intent);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Throwable t) {
+            return false;
+        }
+    }
 }
---
+++ lib/views/flutterBoost/firstRouteWidget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FirstRouteWidget extends StatefulWidget {
+  @override
+  FirstRouteWidgetState createState() => new FirstRouteWidgetState();
+
+}
+
+class FirstRouteWidgetState extends State<FirstRouteWidget> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(child: Text('跳转原生'),);
+  }
+  
+}
